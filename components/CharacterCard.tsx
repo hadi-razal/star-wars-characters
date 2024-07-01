499f32435ec2d9036b6af0f8c4a1171bf094096f
hadi-razal <hadhirasal22@gmail.com>
2024-07-02 00:54:16 +0530

    updates

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -1,29 +1,71 @@
+'use client'
+
 import { CharacterType } from '@/type';
 import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Stack, Text } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/navigation';
+import { FC, useEffect, useState } from 'react';
 import { CiStar } from 'react-icons/ci';
 
 interface CharacterCardProps {
     character: CharacterType;
 }
 
-const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
+/**
+ * Component for displaying character card.
+ * @param character - The character object.
+ * @returns JSX.Element
+ */
+const CharacterCard: FC<CharacterCardProps> = ({ character }: CharacterCardProps): JSX.Element => {
     const { name, height, mass, gender, birth_year, url } = character;
     const router = useRouter();
 
     // Extracting the unique number from the URL
-    const characterId = url.split('/').filter(Boolean).pop(); // Extract the last segment after splitting by '/'
+    const characterId: string = url.split('/').filter(Boolean).pop()!; // Extract the last segment after splitting by '/'
+
+    // State to manage favorite status
+    const [isFavorite, setIsFavorite] = useState<boolean>(false);
+
+    // Check initial favorite status on component mount
+    useEffect(() => {
+        const favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+        setIsFavorite(favorites.includes(characterId));
+    }, [characterId]);
+
+    /**
+     * Function to add to favorites.
+     */
+    const addToFavorite = (): void => {
+        let favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+        if (!favorites.includes(characterId)) {
+            favorites.push(characterId);
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+            setIsFavorite(true); // Update local state to reflect favorite status
+        }
+    };
+
+    /**
+     * Function to remove from favorites.
+     */
+    const removeFromFavorites = (): void => {
+        let favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+        const index = favorites.indexOf(characterId);
+        if (index !== -1) {
+            favorites.splice(index, 1);
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+            setIsFavorite(false); // Update local state to reflect favorite status
+        }
+    };
 
     return (
         <motion.div
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className='w-[300px]  relative hover:scale-105 bg-slate-800 p-4 rounded-lg'
+            className='w-[300px] relative hover:scale-105 transition-transform duration-300 ease-in-out bg-slate-800 p-4 rounded-lg cursor-pointer'
         >
-            <div className='absolute top-3 right-3 cursor-pointer'>
-                <CiStar size={24} />
+            <div onClick={isFavorite ? removeFromFavorites : addToFavorite} className='absolute top-3 right-3 cursor-pointer'>
+                <CiStar color={isFavorite ? 'yellow' : 'gray'} size={24} />
             </div>
 
             <Card maxW='sm' borderRadius='lg' overflow='hidden'>

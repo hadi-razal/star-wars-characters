0eb4457c75be154eca80067e6e5527d1841d256e
hadi-razal <hadhirasal22@gmail.com>
2024-07-02 16:43:27 +0530

    completed

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -5,108 +5,82 @@ import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Stac
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/navigation';
 import { FC, useEffect, useState } from 'react';
-import { CiStar } from 'react-icons/ci';
 import { FaStar } from 'react-icons/fa';
+import { CiStar } from 'react-icons/ci';
 
 interface CharacterCardProps {
-    character: CharacterType;
+  character: CharacterType;
 }
 
 /**
- * Component for displaying character card.
- * @param character - The character object.
- * @returns JSX.Element
+ * Renders a card component for a specific character.
+ *
+ * @param {CharacterCardProps} props - The props object containing the character data.
+ * @returns {JSX.Element} The rendered character card component.
  */
 const CharacterCard: FC<CharacterCardProps> = ({ character }: CharacterCardProps): JSX.Element => {
-    const { name, height, mass, gender, birth_year, url } = character;
-    const router = useRouter();
+  const { name, height, mass, gender, birth_year, url } = character;
+  const router = useRouter();
 
-    // Extracting the unique number from the URL
-    const characterId: string = url.split('/').filter(Boolean).pop()!; // Extract the last segment after splitting by '/'
+  const characterId: string = url.split('/').at(-1) as string;
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
-    // State to manage favorite status
-    const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  useEffect(() => {
+    const favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+    setIsFavorite(favorites.includes(characterId));
+  }, [characterId]);
 
-    // Check initial favorite status on component mount
-    useEffect(() => {
-        const favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
-        setIsFavorite(favorites.includes(characterId));
-    }, [characterId]);
+  const toggleFavorite = (): void => {
+    const favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+    const updatedFavorites: string[] = isFavorite
+      ? favorites.filter((id: string) => id !== characterId)
+      : [...favorites, characterId];
 
-    /**
-     * Function to add to favorites.
-     */
-    const addToFavorite = (): void => {
-        let favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
-        if (!favorites.includes(characterId)) {
-            favorites.push(characterId);
-            localStorage.setItem('favorites', JSON.stringify(favorites));
-            setIsFavorite(true); // Update local state to reflect favorite status
-        }
-    };
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+    setIsFavorite(!isFavorite);
+  };
 
-    /**
-     * Function to remove from favorites.
-     */
-    const removeFromFavorites = (): void => {
-        let favorites: string[] = JSON.parse(localStorage.getItem('favorites') || '[]');
-        const index = favorites.indexOf(characterId);
-        if (index !== -1) {
-            favorites.splice(index, 1);
-            localStorage.setItem('favorites', JSON.stringify(favorites));
-            setIsFavorite(false); // Update local state to reflect favorite status
-        }
-    };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className='w-[300px] relative hover:scale-105 transition-transform duration-300 ease-in-out bg-slate-800 p-4 rounded-lg cursor-pointer'
+    >
+      <button onClick={toggleFavorite} className="  cursor-pointer bg-transparent border-none p-0 m-0">
+        {isFavorite ? <FaStar color="yellow" size={24} /> : <CiStar color="gray" size={24} />}
+      </button>
 
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-            className='w-[300px] relative hover:scale-105 transition-transform duration-300 ease-in-out bg-slate-800 p-4 rounded-lg cursor-pointer'
-        >
-            <Button
-                // variant='ghost'
-               
-                className='absolute top-3 left-3 cursor-pointer'
-            >
-                {isFavorite ? (
-                    <FaStar onClick={removeFromFavorites} color="yellow" size={24} />
-                ) : (
-                    <CiStar onClick={addToFavorite} color="gray" size={24} />
-                )}
+      <Card maxW='sm' borderRadius='lg' overflow='hidden'>
+        <CardBody>
+          <Stack spacing='2'>
+            <Heading textAlign='center' size='md' fontWeight='bold'>
+              {name}
+            </Heading>
+            <Text fontWeight='light' fontSize='sm'>
+              Height: {height} cm
+            </Text>
+            <Text fontWeight='light' fontSize='sm'>
+              Birth Year: {birth_year}
+            </Text>
+            <Text fontWeight='light' fontSize='sm'>
+              Gender: {gender}
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup onClick={() => router.push(`/character/${characterId}`)} mt='2' width='full' className='w-full'>
+            <Button variant='solid' colorScheme='blue' className='bg-gray-900 px-2 py-2 w-full rounded-md'>
+              <Text fontSize='sm' fontWeight='light'>
+                More Details
+              </Text>
             </Button>
-
-            <Card maxW='sm' borderRadius='lg' overflow='hidden'>
-                <CardBody>
-                    <Stack spacing='2'>
-                        <Heading textAlign='center' size='md' fontWeight='bold'>
-                            {name}
-                        </Heading>
-                        <Text fontWeight='light' fontSize='sm'>
-                            Height: {height} cm
-                        </Text>
-                        <Text fontWeight='light' fontSize='sm'>
-                            Birth Year: {birth_year}
-                        </Text>
-                        <Text fontWeight='light' fontSize='sm'>
-                            Gender: {gender}
-                        </Text>
-                    </Stack>
-                </CardBody>
-                <Divider />
-                <CardFooter>
-                    <ButtonGroup onClick={() => router.push(`/character/${characterId}`)} mt='2' width='full' className='w-full'>
-                        <Button variant='solid' colorScheme='blue' className='bg-gray-900 px-2 py-2 w-full rounded-md'>
-                            <Text fontSize='sm' fontWeight='light'>
-                                More Details
-                            </Text>
-                        </Button>
-                    </ButtonGroup>
-                </CardFooter>
-            </Card>
-        </motion.div>
-    );
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+    </motion.div>
+  );
 };
 
 export default CharacterCard;
